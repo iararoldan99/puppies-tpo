d775b202535600cb626e807724613494542dea38
martinkramer04 <martinkramer04@gmail.com>
2026-05-28 17:43:38 -0300

    Feat: Inicialización de Spring Boot

---
+++ src/main/java/com/uade/puppies_tpo/PuppiesTpoApplication.java
@@ -0,0 +1,13 @@
+package com.uade.puppies_tpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppiesTpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppiesTpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uade/puppies_tpo/PuppiesTpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.puppies_tpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppiesTpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
